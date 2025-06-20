7531f214233bb1704388fca80e2de1de5cf73baa
João Grilo <114024705+Arkryvidae@users.noreply.github.com>
2025-06-20 11:40:37 +0100

    WIP

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -14,7 +14,7 @@ const Panel: React.FC<PanelProps> = ({
   as: Component = "div",
 }) => {
   const baseClasses = `
-    bg-light-panel-bg/10 backdrop-blur-md border-2 border-dashed border-neutral-700/50 p-6 rounded-lg panel-with-corners relative flex flex-col h-full items-center justify-center text-center group hover:border-accent/50 animate-glow-shadow transition-all duration-300 w-full
+    bg-light-panel-bg/10 backdrop-blur border-2 border-dashed border-neutral-700/50 p-6 rounded-lg panel-with-corners relative flex flex-col h-full items-center justify-center text-center group hover:border-accent/50 animate-glow-shadow transition-all duration-300 w-full
   `;
 
   return (
