5b70376ffa599d7656fe92b807ac0b3182650ccb
João Grilo <114024705+Arkryvidae@users.noreply.github.com>
2025-06-29 11:21:25 +0100

    Getting Perfomance to 100% Test1

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,10 @@
 import dynamic from "next/dynamic";
 import Hero from "@/components/Hero";
 
+// This export enables Incremental Static Regeneration (ISR)
+// It tells Next.js to pre-build this page and revalidate it at most once per hour.
+export const revalidate = 3600; // Revalidate every hour (3600 seconds)
+
 // Carrega os componentes que são visíveis "above the fold"
 const About = dynamic(() => import("@/components/About"));
 const Skills = dynamic(() => import("@/components/Skills"));
