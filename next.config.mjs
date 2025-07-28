de2be909210af9de146303c1921f7d0c0d22ce28
João Grilo <joao.grilo.dev@gmail.com>
2025-07-28 16:22:12 +0100

    fix(build): Update static export method for Next.js 14

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ import withNextIntl from "next-intl/plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ...(process.env.BUILD_STATIC_EXPORT === "true" && {
-    output: "export",
-  }),
+  ...{
+    output: process.env.BUILD_STATIC_EXPORT === "true" ? "export" : undefined,
+  },
 
   reactStrictMode: true,
   images: {
